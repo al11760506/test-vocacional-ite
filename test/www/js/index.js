3397649fd9cf0d2a433489acefa30446d90553be
ivan <al11760506@ite.edu.mx>
2015-05-25 13:41:23 -0700

    app version 2
    cambios significativos ..casi lista!!

---
+++ test/www/js/index.js
@@ -0,0 +1,116 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts2.html"
+        }
+      }
+    })
+
+     .state('tabs.facts3', {
+      url: "/facts3",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts3.html"
+        }
+      }
+    })
+      .state('tabs.facts4', {
+      url: "/facts4",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts4.html"
+        }
+      }
+    })
+       .state('tabs.facts5', {
+      url: "/facts5",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts5.html"
+        }
+      }
+    })
+        .state('tabs.facts6', {
+      url: "/facts6",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts6.html"
+        }
+      }
+    })
+         .state('tabs.facts7', {
+      url: "/facts7",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts7.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/about.html"
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/nav-stack.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "templates/contact.html"
+        }
+      }
+    });
+
+
+   $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
+});
+
+function MyCtrl($scope, $ionicSlideBoxDelegate) {
+  $scope.nextSlide = function() {
+    $ionicSlideBoxDelegate.next();
+
+  }
+}
